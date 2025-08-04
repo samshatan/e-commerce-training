6258002440aad187eb9ad3beaf6fe1da41294920
Sameer Maurya <sameermaurya97044@gmail.com>
2025-08-04 19:37:48 +0530

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,7 +6,7 @@ import OurPolicy from "@/components/OurPolicy";
 
 function Home(){
   return(
-    <div className="px-4 sm:px-6 lg:px-8">
+    <div className="px-4 sm:px-6 lg:px-8 py-4">
       <HeroSection/>
       <LatestCollection/>
       <BestSeller/>
